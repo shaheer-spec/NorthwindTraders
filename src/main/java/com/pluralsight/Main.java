fd6205620c0c291bbd23c072c4e7e0bdd8a6253d
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-11-24 14:57:41 -0500

    With code

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.sql.*;
+
 public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/northwind",
+                "root",
+                "yearup"
+        );
+
+        Statement statement = connection.createStatement();
+
+        String read = """
+                SELECT ProductName\s
+                FROM northwind.products;
+                """;
+
+        ResultSet lines = statement.executeQuery(read);
+
+        while (lines.next()){
+            String productName = lines.getString(1);
+            System.out.println(productName);
+        }
+        connection.close();
+    }
 }
